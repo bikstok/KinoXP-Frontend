8750bc976deaac0de4347b4d1ec742cfb22e939d
MadsLindersson <Mali0002@stud.kea.dk>
2025-03-07 09:59:32 +0100

    US02 working progress. Pair programming with Jakob. HTML page dynamically showing movies. Next step is dynamically showing movie screenings.

---
+++ KinoXp/KinoEmployees/KinoEmployeesJavaScript/employeeRUDJavaScript.js
@@ -0,0 +1,38 @@
+const listOfMovies = [];
+const showInfoTable = document.querySelector('#showMoviesTable');
+
+fetch('http://localhost:8080/movies')
+    .then(response => {
+        if(!response.ok) {
+            throw new Error('Could nok acces the movie database...');
+        }
+        return response.json();
+    })
+    .then(data => {
+        listOfMovies.push(...data);
+        console.log(listOfMovies);
+        populateTableOfMovies();
+    })
+    .catch(error => {
+        console.error('Could nok acces the movie database...', error);
+    });
+
+
+
+function populateTableOfMovies ()   {
+    listOfMovies.forEach(movie => {
+        let row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${movie.movieTitle}</td>
+        <td>${movie.movieLength}</td>
+        <td>${movie.description}</td>
+        <td>${movie.ageRequirement}</td>
+        <td>${movie.moviePosterUrl}</td>
+        <td><button>Rediger</button><button>Slet</button></td>
+       
+    `;
+
+        showInfoTable.appendChild(row);
+    })
+}
